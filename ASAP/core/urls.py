be2fb0685eae67f6dc33e8e1bd9477e2cbae369c
Daniyal Jawad <125137631+Daniyal323@users.noreply.github.com>
2025-03-03 22:44:33 +0500

    Update urls.py

--- ASAP/core/urls.py
+++ ASAP/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from core.views import index, product_list_view,product_detail_view, category_list_view, category_product_list_view, vendor_list_view, vendor_detail_view,ajax_add_review, search_view,add_to_cart, cart_view,delete_item_from_cart, checkout, payment_completed_view, payment_failed_view
+from core.views import index, product_list_view,product_detail_view, category_list_view, category_product_list_view, vendor_list_view, vendor_detail_view,ajax_add_review, search_view,add_to_cart, cart_view,delete_item_from_cart, checkout, payment_completed_view, payment_failed_view, about_view
 from core import views
 
 app_name = 'core'
@@ -33,5 +33,7 @@ urlpatterns = [
 
     path('payment-failed/', payment_failed_view, name="payment-failed"),
 
-    path('save_checkout_view/',views.save_checkout_view, name="save_checkout_view")
+    path('save_checkout_view/',views.save_checkout_view, name="save_checkout_view"),
+
+    path('about/', about_view, name="about"),
 ]
