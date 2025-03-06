9a36f3eea2c9605c4e42e4ae2c518dc5c82f94f1
Daniyal Jawad <125137631+Daniyal323@users.noreply.github.com>
2025-03-06 23:15:35 +0500

    Update forms.py

--- ASAP/core/forms.py
+++ ASAP/core/forms.py
@@ -1,5 +1,5 @@
 from django import forms 
-from core.models import ProductReview
+from core.models import ProductReview, Seller
 
 class ProductReviewForm(forms.ModelForm):
     review = forms.CharField(widget=forms.Textarea(attrs = {'placeholder': "Write review"}))
@@ -7,3 +7,8 @@ class ProductReviewForm(forms.ModelForm):
     class Meta:
         model = ProductReview
         fields = ['review', 'rating']
+
+class SellerRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Seller
+        fields = ['business_name', 'business_address', 'phone']
